69fba06d4c2355ccf510e4e8ddf79a6f6ce34bf9
Huzefa Dohadwala <104446701+HuzefaDohadwala@users.noreply.github.com>
2022-11-03 22:34:57 +0530

    Update merege sort_Prolog.pl

--- merege sort_Prolog.pl
+++ merege sort_Prolog.pl
@@ -6,3 +6,5 @@ merge([MinList1|RestMerged], [MinList1|RestList1], [MinList2|RestList2]) :-
 merge([MinList2|RestMerged], [MinList1|RestList1], [MinList2|RestList2]) :-
   MinList2 =< MinList1,
   merge(RestMerged,[MinList1|RestList1],RestList2).
+  
+  merge(What, [1, 3, 5, 6], [2, 4, 7]).
